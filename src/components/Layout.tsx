423978d854b8813dddce5065cd6f60535db06979
Nguyễn Tấn Thoại <thoaint@bap.jp>
2023-01-04 14:27:13 +0700

    update

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -12,7 +12,7 @@ export default function Layout({ children }: Props) {
   useAuthenticate()
   return (
     <VStack h="100vh" background="main.1" w="100vw">
-      <VStack flex={1} width={['sm', 'md']}>
+      <VStack width={['sm', 'md']} height="100%">
         <Header hasBack />
         <VStack flex={1} overflow="scroll" className={styles.mt0} w="100%">
           {children}
